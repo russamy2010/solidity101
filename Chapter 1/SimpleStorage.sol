7c4a1fd62f958d51db0fb891e9e6461ffa72f0f8
russamy2010 <amymruss@gmail.com>
2025-05-26 14:08:16 -0400

    Created file and notes for SimpleStorage contract

---
+++ Chapter 1/SimpleStorage.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18; //stating the version-->will work with anything above 0.8.18
+///can use range notation as well
+
+contract SimpleStorae{
+    
+}
